32a33434a2f423cd246eaec56852c88b1fdcf0e2
Alex <alexnova98@gmail.com>
2021-04-18 22:23:45 +0300

    finishing the ex, just fix the throw

--- NumberWithUnits.cpp
+++ NumberWithUnits.cpp
@@ -1,75 +1,155 @@
 #include "NumberWithUnits.hpp"
 #include <iostream>
 #include <string>
+#include <stdexcept>
+#include <fstream>
+#include <tuple>
+
 
 using namespace std;
 
 namespace ariel {
-    // TODO fix all operators to work with all legit types.
-    void NumberWithUnits::read_units(ifstream &file) {
-        cout << "Hello World!" << endl;
+    double getUnitSize(const string &unitLeft, const string &unitRight) {
+        double retSize = 1;
+        string unit = unitLeft;
+        bool endOfMap = false;
+        while (!endOfMap) {
+            if (unitRight == unit) {
+                return retSize;
+            }
+            if (unitsAvailable.find(unit) != unitsAvailable.end()) {
+                retSize /= get<1>(unitsAvailable[unit]);
+                unit = get<0>(unitsAvailable[unit]);
+            } else {
+                endOfMap = true;
+            }
+        }
+        retSize = 1;
+        endOfMap = false;
+        unit = unitRight;
+        while (!endOfMap) {
+            if (unitLeft == unit) {
+                return retSize;
+            }
+            if (unitsAvailable.find(unit) != unitsAvailable.end()) {
+                retSize *= get<1>(unitsAvailable[unit]);
+                unit = get<0>(unitsAvailable[unit]);
+            } else {
+                endOfMap = true;
+            }
+        }
+        return -1;
     }
 
-    std::ostream &operator<<(std::ostream &os, const NumberWithUnits &unit) {
+
+    void NumberWithUnits::read_units(ifstream &file) {
+        if (file.fail() || file.eof() || file.bad()) {
+            throw std::runtime_error("Error in opening file");
+        }
+        file.clear();
+        file.seekg(0);
+        // This while loop adds the units to the map
+        while (!file.eof()) {
+            string firstUnitName;
+            string secondUnitName;
+            double secondUnit = 0;
+
+            file.ignore(2) >> firstUnitName;
+            file.ignore(2) >> secondUnit >> secondUnitName;
+            file.ignore(2);
+            unitsAvailable[firstUnitName] = make_tuple(secondUnitName, secondUnit);
+
+            /// ===adding to set here=====
+            validUnits.insert(firstUnitName);
+            validUnits.insert(secondUnitName);
+            /// ===adding to set here=====
+
+        }
+        file.clear();
+        file.seekg(0);
+    }
+
+    ostream &operator<<(std::ostream &os, const NumberWithUnits &unit) {
         os << unit.num_ << "[" << unit.str_ << ']';
         return os;
     }
 
-    std::istream &operator>>(std::istream &is, NumberWithUnits &unit) {
+    istream &operator>>(std::istream &is, NumberWithUnits &unit) {
         is >> unit.num_;
+        is.ignore(2);
+        is >> unit.str_;
         return is;
     }
 
     NumberWithUnits operator-(const NumberWithUnits &unit1, const NumberWithUnits &unit2) {
-        return NumberWithUnits(unit1.num_-unit2.num_, unit1.str_);
+        double sizeOfUnit = getUnitSize(unit1.str_, unit2.str_);
+        if (sizeOfUnit == -1) {
+            throw runtime_error("Wrong type of units, cant operate");
+        }
+        return NumberWithUnits(unit1.num_ - unit2.num_ * sizeOfUnit, unit1.str_);
     }
 
     NumberWithUnits operator-(const NumberWithUnits &unit) {
         return NumberWithUnits(-unit.num_, unit.str_);
     }
 
-    NumberWithUnits operator--(const NumberWithUnits &unit) {
-        return NumberWithUnits(0, "");
+    NumberWithUnits operator--(NumberWithUnits &unit) {
+        unit.num_--;
+        return unit;
     }
 
-    NumberWithUnits operator--(const NumberWithUnits &unit, int) {
-        return NumberWithUnits(0, "");
+    NumberWithUnits operator--(NumberWithUnits &unit, int) {
+        --unit.num_;
+        return unit;
     }
 
     NumberWithUnits operator+(const NumberWithUnits &unit1, const NumberWithUnits &unit2) {
-        return NumberWithUnits(unit1.num_ + unit2.num_, unit1.str_);
+        double sizeOfUnit = getUnitSize(unit1.str_, unit2.str_);
+        if (sizeOfUnit == -1) {
+            throw runtime_error("Wrong type of units, cant operate");
+        }
+        return NumberWithUnits(unit1.num_ + unit2.num_ * sizeOfUnit, unit1.str_);
     }
 
     NumberWithUnits operator+(const NumberWithUnits &unit) {
-        return NumberWithUnits(0, "");
+        return NumberWithUnits(+unit.num_, unit.str_);
     }
 
-    NumberWithUnits operator++(const NumberWithUnits &unit) {
-        return NumberWithUnits(0, "");
+    NumberWithUnits operator++(NumberWithUnits &unit) {
+        unit.num_++;
+        return unit;
     }
 
-    NumberWithUnits operator++(const NumberWithUnits &unit, int) {
-        return NumberWithUnits(0, "");
+    NumberWithUnits operator++(NumberWithUnits &unit, int) {
+        ++unit.num_;
+        return unit;
     }
 
     NumberWithUnits operator*(const NumberWithUnits &unit, double val) {
-        return NumberWithUnits(0, "");
+        return NumberWithUnits(unit.num_ * val, unit.str_);
     }
 
     NumberWithUnits operator*(double val, const NumberWithUnits &unit) {
         return NumberWithUnits(val * unit.num_, unit.str_);
     }
 
-    NumberWithUnits operator+=(const NumberWithUnits &unit1, const NumberWithUnits &unit2) {
-        return NumberWithUnits(0, "");
+    NumberWithUnits operator+=(NumberWithUnits &unit1, const NumberWithUnits &unit2) {
+        return unit1 = unit1 + unit2;
     }
 
-    NumberWithUnits operator-=(const NumberWithUnits &unit1, const NumberWithUnits &unit2) {
-        return NumberWithUnits(0, "");
+    NumberWithUnits operator-=(NumberWithUnits &unit1, const NumberWithUnits &unit2) {
+        return unit1 = unit1 - unit2;
     }
 
     bool operator==(const NumberWithUnits &unit1, const NumberWithUnits &unit2) {
-        return unit1.num_ == unit2.num_ && unit1.str_ == unit2.str_;
+        double sizeOfUnit = getUnitSize(unit1.str_, unit2.str_);
+        if (sizeOfUnit == -1) {
+            throw runtime_error("Wrong type of units, cant operate");
+        }
+
+        double temp = sizeOfUnit * unit2.num_;
+        return temp == unit1.num_;
+
     }
 
     bool operator!=(const NumberWithUnits &unit1, const NumberWithUnits &unit2) {
@@ -77,19 +157,40 @@ namespace ariel {
     }
 
     bool operator<(const NumberWithUnits &unit1, const NumberWithUnits &unit2) {
-        return true;
+        double sizeOfUnit = getUnitSize(unit1.str_, unit2.str_);
+        if (sizeOfUnit == -1) {
+            throw runtime_error("Wrong type of units, cant operate");
+        }
+        double temp = sizeOfUnit * unit2.num_;
+        return unit1.num_ < temp;
+
     }
 
     bool operator>(const NumberWithUnits &unit1, const NumberWithUnits &unit2) {
-        return true;
+        double sizeOfUnit = getUnitSize(unit1.str_, unit2.str_);
+        if (sizeOfUnit == -1) {
+            throw runtime_error("Wrong type of units, cant operate");
+        }
+        double temp = sizeOfUnit * unit2.num_;
+        return unit1.num_ > temp;
+
     }
 
     bool operator<=(const NumberWithUnits &unit1, const NumberWithUnits &unit2) {
-        return true;
+        double sizeOfUnit = getUnitSize(unit1.str_, unit2.str_);
+        if (sizeOfUnit == -1) {
+            throw runtime_error("Wrong type of units, cant operate");
+        }
+        double temp = sizeOfUnit * unit2.num_;
+        return unit1.num_ <= temp;
     }
 
     bool operator>=(const NumberWithUnits &unit1, const NumberWithUnits &unit2) {
-        return true;
+        double sizeOfUnit = getUnitSize(unit1.str_, unit2.str_);
+        if (sizeOfUnit == -1) {
+            throw runtime_error("Wrong type of units, cant operate");
+        }
+        double temp = sizeOfUnit * unit2.num_;
+        return unit1.num_ >= temp;
     }
-
 }
--- NumberWithUnits.hpp
+++ NumberWithUnits.hpp
@@ -5,16 +5,25 @@
 #include <iostream>
 #include <stdexcept>
 #include <map>
+#include <set>
 #include <tuple>
 
 namespace ariel {
+    static std::map <std::string, std::tuple<std::string, double>> unitsAvailable;
+    static std::set <std::string> validUnits;
     class NumberWithUnits {
     private:
         double num_;
         std::string str_;
-        std::map<std::string, std::tuple<std::string, double>> unitsAvailable;
     public:
         NumberWithUnits(double num, const std::string &str) {
+            // todo check why set is empty!
+//            if (validUnits.empty()) {
+//                std::cout << "set is empty!" << std::endl;
+//            }
+//            if (!(validUnits.find(str) != validUnits.end())) {
+//                throw std::runtime_error("Can not find the unit requested");
+//            }
             num_ = num;
             str_ = str;
         }
@@ -34,9 +43,9 @@ namespace ariel {
 
         friend NumberWithUnits operator-(const NumberWithUnits &unit);
 
-        friend NumberWithUnits operator--(const NumberWithUnits &unit, int);
+        friend NumberWithUnits operator--(NumberWithUnits &unit, int);
 
-        friend NumberWithUnits operator--(const NumberWithUnits &unit);
+        friend NumberWithUnits operator--(NumberWithUnits &unit);
         //<<<<<<<================== -/-- end   ==================>>>>>>>
 
         //<<<<<<<================== +/++ start ==================>>>>>>>
@@ -44,22 +53,23 @@ namespace ariel {
 
         friend NumberWithUnits operator+(const NumberWithUnits &unit);
 
-        friend NumberWithUnits operator++(const NumberWithUnits &unit);
+        friend NumberWithUnits operator++(NumberWithUnits &unit);
 
-        friend NumberWithUnits operator++(const NumberWithUnits &unit, int);
+        friend NumberWithUnits operator++(NumberWithUnits &unit, int);
         //<<<<<<<================== +/++ end   ==================>>>>>>>
 
         //<<<<<<<================== * start ==================>>>>>>>
         friend NumberWithUnits operator*(const NumberWithUnits &unit, double val);
-        friend NumberWithUnits operator*( double val, const NumberWithUnits &unit);
+
+        friend NumberWithUnits operator*(double val, const NumberWithUnits &unit);
         //<<<<<<<================== * end   ==================>>>>>>>
 
         //<<<<<<<================== += start ==================>>>>>>>
-        friend NumberWithUnits operator+=(const NumberWithUnits &unit1, const NumberWithUnits &unit2);
+        friend NumberWithUnits operator+=(NumberWithUnits &unit1, const NumberWithUnits &unit2);
         //<<<<<<<================== += end   ==================>>>>>>>
 
         //<<<<<<<================== -= start ==================>>>>>>>
-        friend NumberWithUnits operator-=(const NumberWithUnits &unit1, const NumberWithUnits &unit2);
+        friend NumberWithUnits operator-=(NumberWithUnits &unit1, const NumberWithUnits &unit2);
         //<<<<<<<================== -= end   ==================>>>>>>>
 
         //<<<<<<<================== == start ==================>>>>>>>
